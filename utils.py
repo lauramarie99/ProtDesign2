f18e93ea4971dab10f127e25359a626ae6b352dd
lauramarie99 <lauramarie.pilgram@web.de>
2024-09-10 11:18:24 +0200

    return stdout from subprocess

--- utils.py
+++ utils.py
@@ -2,10 +2,13 @@ import subprocess
 
 # Run subprocess
 def run(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, text=True)
     while True:
         line = process.stdout.readline()
-        if not line: break    
+        if line:
+            print(f"Output: {line.strip()}")
+        else: 
+            break    
     return_code = process.wait()
 
 
