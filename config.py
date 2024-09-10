0f9229830cbc77c18d55abd5365f8d61aaafc84b
lauramarie99 <lauramarie.pilgram@web.de>
2024-09-10 11:19:32 +0200

    add comments

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-# Define paths and other configuration variables
+# Define paths to repositories
 RFDIFFUSION_PATH = "RFdiffusion"
 RFDIFFUSIONAA_PATH = "rf_diffusion_all_atom"
 LIGANDMPNN_PATH = "LigandMPNN"
 
 # To run on cluster
-REPO_PATH = ""
-RFDIFFUSION_CONTAINER = ""
+REPO_PATH = ""                              # Path to ProtDesign2 Repo
+RFDIFFUSION_CONTAINER = ""                  # Paths to singularity container
 RFDIFFUSIONAA_CONTAINER = ""
 SEQDESIGN_CONTAINER = ""
 FOLDING_CONTAINER = ""
-EMAIL = ""
-PARTITION = "paula"
-GPU = "a30:1"
-COLABFOLD_WEIGHTS = ""
+EMAIL = ""                                  # Email for slurm notifications
+PARTITION = "paula"                         # Partition to use for runs
+GPU = "a30:1"                               # GPU to use
+COLABFOLD_WEIGHTS = ""                      # Path to colabfold weights
 
