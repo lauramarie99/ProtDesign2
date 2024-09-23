933a98fc884acaf4d72d782d91e2ff574986eaee
lauramarie99 <lauramarie.pilgram@web.de>
2024-09-23 10:51:04 +0200

    remove cst file generation

--- create_configs.py
+++ create_configs.py
@@ -38,26 +38,6 @@ for n in range(args.num_contigs):
     new_config_args["diffusion"]["contigs"] = new_contig
     new_config_args["diffusion"]["name"] = f"{config_args['diffusion']['name']}{n+1}"
 
-    if 'cst_file' in config_args['seqdesign']:
-        model_motif, ref_motif = utils.get_motifs(new_contig)
-        model_motif = [(resi[1:]+resi[0]) for resi in model_motif]
-        print(model_motif)
-        ref_motif = [(resi[1:]+resi[0]) for resi in ref_motif]
-        print(ref_motif)
-        with open(config_args['seqdesign']['cst_file']) as infile:
-            data = infile.readlines()
-        new_data = []
-        for line in data:
-            new_line = line
-            for i, resi in enumerate(ref_motif):
-                if resi in line:
-                    new_line = new_line.replace(resi, model_motif[i])
-            new_data.append(new_line)
-        with open(f"{args.outdir}/{name}_{n+1}.cst", "w") as outfile:
-            outfile.writelines(new_data)
-
-        new_config_args["seqdesign"]["cst_file"] = f"{args.outdir}/{name}_{n+1}.cst"
-
     with open(f"{args.outdir}/{name}_{n+1}.yml", "w") as outfile:
         yaml.dump(new_config_args, outfile)
 
